d8e1040f789a1a93be724533184a217f62894508
saffbran <bsaffert@my.wctc.edu>
2019-01-22 16:59:24 -0600

    Adding Home Controller and Views Folder

---
+++ Northwind.Core2/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Core2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() => "This is Lambda";
+    }
+}
